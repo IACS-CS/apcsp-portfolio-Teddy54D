17e83198bd7acb92c5d5870f16b7318f2ca1b985
Teddy54D <edward.darlington@innovationcharter.org>
2024-10-07 16:24:48 +0000

    Weird particles stuff -TD

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 import "../styles/styles.css";
 import "@picocss/pico/css/pico.min.css";
-import * as THREE from 'three';
+//import particlesJS from 'particles.js';
 // Add imports here
 
 console.log("What?");
 console.log("Hello world, from main.js!");
-if (typeof window !== "undefined") {
+
+if (typeof window !== 'undefined') {
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
   //
   // If you see a SSR error, try moving your code here!
-  console.log("Hello world, browser :-)");
 }
+
+
